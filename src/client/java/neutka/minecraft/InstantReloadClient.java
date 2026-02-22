d199c93242e3151c7d5a6738de54c9bdae0950e0
ne-utka <lebedev.vadim.03@mail.ru>
2026-02-22 18:18:54 +0300

    Init

---
+++ src/client/java/neutka/minecraft/InstantReloadClient.java
@@ -0,0 +1,10 @@
+package neutka.minecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InstantReloadClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
